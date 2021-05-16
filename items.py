eafc95269dc39472ecdd8ebf908998ddcb42d581
Stefan Horst <stefan@ultrachaos.de>
2021-05-16 16:12:41 +0200

    move key tu trusted.d folder

--- items.py
+++ items.py
@@ -1,33 +1,30 @@
 actions = {
-    'install_java_key': {
-        'cascade_skip': False,
-        'command': 'apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886',
-        'unless': 'apt-key list | grep "EEA1 4886"',
-        'needs': ['pkg_apt:dirmngr'],
-    },
     'accept_java_license': {
-        'command': 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections',
+        'command': 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | '
+                   '/usr/bin/debconf-set-selections',
         'unless': 'debconf-show oracle-java8-installer | grep accepted-oracle-license-v1-1'
     }
 }
+files = {}
+
+if node.has_bundle('apt'):
+    # this is a ubuntu only repo, so wi install trusty on debain
+    release = node.metadata.get('ubuntu', {}).get('release_name', 'trusty')
+
+    files['/etc/apt/sources.list.d/webupd8team-java.list'] = {
+        'content': f'deb http://ppa.launchpad.net/webupd8team/java/ubuntu {release} main\n',
+        'content_type': 'text',
+        'needs': ['file:/etc/apt/trusted.gpg.d/webupd8team-java.gpg', ],
+        'triggers': ["action:force_update_apt_cache", ],
+    }
+
+    files['/etc/apt/trusted.gpg.d/webupd8team-java.gpg'] = {
+        'content_type': 'binary',
+    }
 
 pkg_apt = {
-    'dirmngr': {
-    },
     'oracle-java8-installer': {
         'needs': ['action:accept_java_license'],
         'cascade_skip': False
-    }
-}
-
-files = {
-    '/etc/apt/sources.list.d/webupd8team-java.list': {
-        'source': "webupd8team-java.list",
-        'content_type': "mako",
-        'owner': "root",
-        'group': "root",
-        'mode': "0444",
-        'needs': ['action:install_java_key'],
-        'triggers': ['action:update_apt_cache']
     }
 }
