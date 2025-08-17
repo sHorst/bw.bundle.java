40cd6a8362a1df1e6945c8f8b7666d3340eab934
Stefan Horst <stefan@ultrachaos.de>
2025-08-17 11:07:19 +0200

    move to openjdk for debian > 12

--- items.py
+++ items.py
@@ -1,30 +1,48 @@
-actions = {
-    'accept_java_license': {
-        'command': 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | '
-                   '/usr/bin/debconf-set-selections',
-        'unless': 'debconf-show oracle-java8-installer | grep accepted-oracle-license-v1-1'
-    }
-}
 files = {}
 
 if node.has_bundle('apt'):
-    # this is a ubuntu only repo, so wi install trusty on debain
-    release = node.metadata.get('ubuntu', {}).get('release_name', 'trusty')
+    if node.os == 'debian' and node.os_version[0] >= 12:
+        pkg_apt = {
+            'openjdk-11-jre': {
+                'installed': True,
+                'tags': ["provides-java"],
+            },
+            'oracle-java8-installer': {
+                'installed': False,
+                'tags': ["provides-java"],
+            }
+        }
+        files['/etc/apt/trusted.gpg.d/webupd8team-java.gpg'] = {'delete': True}
+        files['/etc/apt/sources.list.d/webupd8team-java.list'] = {
+            'delete': True,
+            'triggers': ["action:force_update_apt_cache", ],
+        }
+    else:
+        # this is a ubuntu only repo, so wi install trusty on debain
+        release = node.metadata.get('ubuntu', {}).get('release_name', 'trusty')
+        actions = {
+            'accept_java_license': {
+                'command': 'echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | '
+                           '/usr/bin/debconf-set-selections',
+                'unless': 'debconf-show oracle-java8-installer | grep accepted-oracle-license-v1-1'
+            }
+        }
 
-    files['/etc/apt/sources.list.d/webupd8team-java.list'] = {
-        'content': f'deb http://ppa.launchpad.net/webupd8team/java/ubuntu {release} main\n',
-        'content_type': 'text',
-        'needs': ['file:/etc/apt/trusted.gpg.d/webupd8team-java.gpg', ],
-        'triggers': ["action:force_update_apt_cache", ],
-    }
+        files['/etc/apt/sources.list.d/webupd8team-java.list'] = {
+            'content': f'deb http://ppa.launchpad.net/webupd8team/java/ubuntu {release} main\n',
+            'content_type': 'text',
+            'needs': ['file:/etc/apt/trusted.gpg.d/webupd8team-java.gpg', ],
+            'triggers': ["action:force_update_apt_cache", ],
+        }
 
-    files['/etc/apt/trusted.gpg.d/webupd8team-java.gpg'] = {
-        'content_type': 'binary',
-    }
+        files['/etc/apt/trusted.gpg.d/webupd8team-java.gpg'] = {
+            'content_type': 'binary',
+        }
 
-pkg_apt = {
-    'oracle-java8-installer': {
-        'needs': ['action:accept_java_license'],
-        'cascade_skip': False
-    }
-}
+        pkg_apt = {
+            'oracle-java8-installer': {
+                'needs': ['action:accept_java_license'],
+                'cascade_skip': False,
+                'tags': ["provides-java"],
+            }
+        }
